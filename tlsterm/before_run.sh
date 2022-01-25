d9a84b98f9e65fb16dd50b6ccb4342ab95a2029f
yeranosyanvahan <69711561+yeranosyanvahan@users.noreply.github.com>
2022-01-25 17:15:15 +0400

    dumping everything to before_run.sh

--- tlsterm/before_run.sh
+++ tlsterm/before_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/tlsterm/certs/server.key -out /etc/tlsterm/certs/server.crt -subj "/C=/ST=/L=/O=/OU=/CN="
+/usr/bin/tlsterm
