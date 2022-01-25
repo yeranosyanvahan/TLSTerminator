47b5300a60eaf8fe0464141af540dc67a41f5784
yeranosyanvahan <69711561+yeranosyanvahan@users.noreply.github.com>
2022-01-25 16:48:20 +0400

    Fixing panics

--- tlsterm/proxy.go
+++ tlsterm/proxy.go
@@ -8,15 +8,15 @@ import (
 )
 
 type Proxy struct {
-	Validate 		bool
-	AllowIPs    	[]net.IP
-	IN 				endpoint
-	OUT 			endpoint
-	Redirect        string
-	SSLCRTOUTFILE	string
-	SSLKEYOUTFILE	string
-	SSLCRTINFILE	string
-	SSLKEYINFILE	string
+	Validate      bool
+	AllowIPs      []net.IP
+	IN            endpoint
+	OUT           endpoint
+	Redirect      string
+	SSLCRTOUTFILE string
+	SSLKEYOUTFILE string
+	SSLCRTINFILE  string
+	SSLKEYINFILE  string
 }
 
 func LoadProxy(host string, section *ini.Section) (Proxy, error) {
@@ -33,8 +33,8 @@ func LoadProxy(host string, section *ini.Section) (Proxy, error) {
 	if err != nil {
 		return proxy, err
 	}
-	if (err != nil) {
-		return proxy,  errors.New(host + " Loading Certificate Error "+ err.Error())
+	if err != nil {
+		return proxy, errors.New(host + " Loading Certificate Error " + err.Error())
 	}
 	proxy.IN = EndpointIN
 	proxy.OUT = EndpointOUT
@@ -49,47 +49,57 @@ func (proxy Proxy) GETOUTCerts() (tls.Certificate, error) {
 	return tls.LoadX509KeyPair(proxy.SSLCRTOUTFILE, proxy.SSLKEYOUTFILE)
 }
 func (proxy Proxy) CheckSSL(globals Global) error {
-	if(globals.TLSIN){
-		_,err := proxy.GETINCerts()
-		if err!= nil {return err}
+	if globals.TLSIN {
+		_, err := proxy.GETINCerts()
+		if err != nil {
+			return err
+		}
 	}
-	if(globals.TLSIN){
-		_,err := proxy.GETINCerts()
-		if err != nil {return err}
+	if globals.TLSIN {
+		_, err := proxy.GETINCerts()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 
 }
 func (proxy *Proxy) OVERWRITENULL(defaultproxy Proxy) {
-	if(proxy.SSLCRTOUTFILE==""){
-		proxy.SSLCRTOUTFILE=defaultproxy.SSLCRTOUTFILE
+	if proxy.SSLCRTOUTFILE == "" {
+		proxy.SSLCRTOUTFILE = defaultproxy.SSLCRTOUTFILE
 	}
-	if(proxy.SSLKEYOUTFILE==""){
-		proxy.SSLKEYOUTFILE=defaultproxy.SSLKEYOUTFILE
+	if proxy.SSLKEYOUTFILE == "" {
+		proxy.SSLKEYOUTFILE = defaultproxy.SSLKEYOUTFILE
 	}
-	if(proxy.SSLCRTINFILE==""){
-		proxy.SSLCRTINFILE=defaultproxy.SSLCRTINFILE
+	if proxy.SSLCRTINFILE == "" {
+		proxy.SSLCRTINFILE = defaultproxy.SSLCRTINFILE
 	}
-	if(proxy.SSLKEYINFILE==""){
-		proxy.SSLKEYINFILE=defaultproxy.SSLKEYINFILE
+	if proxy.SSLKEYINFILE == "" {
+		proxy.SSLKEYINFILE = defaultproxy.SSLKEYINFILE
 	}
-	if(proxy.Redirect==""){
-		proxy.Redirect=defaultproxy.Redirect
+	if proxy.Redirect == "" {
+		proxy.Redirect = defaultproxy.Redirect
 	}
 }
 
 func (proxy *Proxy) CheckConnection(globals Global) error {
 
-	if(global.TLSOUT) {
-		config := &tls.Config{GetCertificate: HandleCertificateOUT, ServerName:proxy.OUT.HostName}
+	if global.TLSOUT {
+		config := &tls.Config{GetCertificate: HandleCertificateOUT, ServerName: proxy.OUT.HostName}
 		serverconn, err := tls.Dial("tcp", proxy.OUT.Addr, config)
-		serverconn.Handshake()
-		serverconn.Close()
+		if err == nil {
+			err = serverconn.Handshake()
+			if err == nil {
+				serverconn.Close()
+			}
+			return err
+		}
 		return err
-	}else{
+	} else {
 		serverconn, err := net.Dial("tcp", proxy.OUT.Addr)
-		serverconn.Close()
+		if err == nil {
+			serverconn.Close()
+		}
 		return err
 	}
 }
-
