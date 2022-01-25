aaf5d467bb3c695ecc2b0edc819b3298da14d9c3
yeranosyanvahan <69711561+yeranosyanvahan@users.noreply.github.com>
2022-01-25 17:16:41 +0400

    Sleeping before executions

--- tlsterm/before_run.sh
+++ tlsterm/before_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/tlsterm/certs/server.key -out /etc/tlsterm/certs/server.crt -subj "/C=/ST=/L=/O=/OU=/CN="
+sleep 1
 /usr/bin/tlsterm
