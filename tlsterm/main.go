0ce2bda51fc6461bdf853a14684418f6944c8a52
yeranosyanvahan <69711561+yeranosyanvahan@users.noreply.github.com>
2022-02-20 20:52:01 +0400

    unexpected { at end of statement

--- tlsterm/main.go
+++ tlsterm/main.go
@@ -143,7 +143,7 @@ func HandleConnection(ServerName string, clientconn net.Conn) {
 	} else {
 		proxy = &defaultproxy
 		if proxy.OUT.Port == "" {
-			log.Println("Could't Find redirect for this host, and no default redirect is found"){
+			log.Println("Could't Find redirect for this host, and no default redirect is found")
 			log.Println("Try connection: ",ServerName+"@"+Port,"vproxies: ", vproxies)
 			return
 		}
